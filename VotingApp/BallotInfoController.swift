db438b4a6cc04fbea3d395c7fc0607bf03a9853e
Jason Ho <raktos@uw.edu>
2015-12-10 19:32:01 -0800

    Functional prototype of parse usage
    Did everything break? Revert back to this!

--- VotingApp/BallotInfoController.swift
+++ VotingApp/BallotInfoController.swift
@@ -7,18 +7,60 @@
 //
 
 import UIKit
+import Parse
+
+struct Ballot {
+    let measures: [String]
+    let closingDate: NSDate
+    let desc: String
+    let title: String
+}
 
 class BallotInfoController: UIViewController {
 
     @IBOutlet weak var startVoteButton: UIButton!
     @IBOutlet weak var ballotInfo: UILabel!
+    @IBOutlet weak var ballotTitle: UILabel!
+    @IBOutlet weak var closingDate: UILabel!
     
     
+    var ballot: Ballot? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+        let parseQuery = PFQuery(className: "ballots")
+        parseQuery.whereKey("closingDate", greaterThan: NSDate()).orderByAscending("closingDate")
+        
+        parseQuery.getFirstObjectInBackgroundWithBlock {
+            (object: PFObject?, error: NSError?) -> Void in
+            if let parseBallot = object {
+                let measureRelation: PFRelation = parseBallot["measures"] as! PFRelation
+                let closingDate: NSDate = parseBallot["closingDate"] as! NSDate
+                let title: String = parseBallot["title"] as! String
+                let description: String = parseBallot["description"] as! String
+                
+                self.ballot = Ballot(measures: ["test", "test"], closingDate: closingDate, desc: description, title: title)
+                
+                self.setLabels(self.ballot!)
+                
+                measureRelation.query().findObjectsInBackgroundWithBlock {
+                    (objects: [PFObject]?, error: NSError?) -> Void in
+                    if let measures = objects {
+                        print(measures)
+                    }
+                }
+            }
+            
+        }
+        
+    }
+    
+    func setLabels(ballot: Ballot) {
+        self.ballotInfo.text = ballot.desc
+        self.ballotTitle.text = ballot.title
+        self.closingDate.text = ballot.closingDate.description
     }
 
     override func didReceiveMemoryWarning() {
