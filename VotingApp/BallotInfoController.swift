b2207d72311127652dbdce60c40f6a0664a9a183
Caleb Thorsteinson <caleb@thorsteinson.me>
2015-12-12 13:26:55 -0800

    Added smart loading for the application
    Instead of loading the application ahead of time, and not allowing the user to go to the next view, or loading it when they need it, making them wait, I found a way to load ahead of time from the menu controller, and then update the UI asnchrnously if it hasn't loaded, or instantly update the view if it has!
    
    This requires making a reference to the child/future controller, but it appears to work well.

--- VotingApp/BallotInfoController.swift
+++ VotingApp/BallotInfoController.swift
@@ -23,44 +23,25 @@ class BallotInfoController: UIViewController {
     @IBOutlet weak var ballotTitle: UILabel!
     @IBOutlet weak var closingDate: UILabel!
     
-    
     var ballot: Ballot? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
-        let parseQuery = PFQuery(className: "ballots")
-        parseQuery.whereKey("closingDate", greaterThan: NSDate()).orderByAscending("closingDate")
-        
-        parseQuery.getFirstObjectInBackgroundWithBlock {
-            (object: PFObject?, error: NSError?) -> Void in
-            if let parseBallot = object {
-                let measureRelation: PFRelation = parseBallot["measures"] as! PFRelation
-                let closingDate: NSDate = parseBallot["closingDate"] as! NSDate
-                let title: String = parseBallot["title"] as! String
-                let description: String = parseBallot["description"] as! String
-                
-                self.ballot = Ballot(measures: ["test", "test"], closingDate: closingDate, desc: description, title: title)
-                
-                self.setLabels(self.ballot!)
-                
-                measureRelation.query().findObjectsInBackgroundWithBlock {
-                    (objects: [PFObject]?, error: NSError?) -> Void in
-                    if let measures = objects {
-                        print(measures)
-                    }
-                }
-            }
-            
-        }
-        
     }
     
-    func setLabels(ballot: Ballot) {
-        self.ballotInfo.text = ballot.desc
-        self.ballotTitle.text = ballot.title
-        self.closingDate.text = ballot.closingDate.description
+    override func viewWillAppear(animated: Bool) {
+        self.setLabels()
+    }
+    
+    func setLabels() {
+        // Don't try this unless it's loaded, otherwise it will attempt to unwrap optional UI Elements
+        if self.isViewLoaded() {
+            if let b = self.ballot {
+                self.ballotInfo.text = b.desc
+                self.ballotTitle.text = b.title
+                self.closingDate.text = b.closingDate.description
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -69,14 +50,9 @@ class BallotInfoController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
-    */
-
 }
