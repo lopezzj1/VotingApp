61f8f54fab6495fd2eb82fc5a2580da20aaf51a3
Caleb Thorsteinson <caleb@thorsteinson.me>
2015-12-12 15:05:58 -0800

    Measures now load in the view

--- VotingApp/BallotInfoController.swift
+++ VotingApp/BallotInfoController.swift
@@ -9,13 +9,6 @@
 import UIKit
 import Parse
 
-struct Ballot {
-    let measures: [Measure]?
-    let measureRelation: PFRelation
-    let closingDate: NSDate
-    let desc: String
-    let title: String
-}
 
 class BallotInfoController: UIViewController {
 
@@ -25,9 +18,33 @@ class BallotInfoController: UIViewController {
     @IBOutlet weak var closingDate: UILabel!
     
     var ballot: Ballot? = nil
+    var measures: [Measure]? = nil
+    var tableCtrl: BallotTableController? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Start trying to load the measures ahead of time
+        if let ballot = self.ballot {
+            ballot.measureRelation.query().findObjectsInBackgroundWithBlock {
+                (objects: [PFObject]?, error: NSError?) -> Void in
+                if let pMeasures = objects {
+                    let measures = pMeasures.map {
+                        (object: PFObject) -> Measure in
+                        let title = object["measureTitle"] as! String
+                        let candidates = object["candidates"] as! PFRelation
+                        return Measure(title: title, candidates: candidates)
+                    }
+                    if let ctrl = self.tableCtrl {
+                        // The table will suddenly populate with data
+                        ctrl.measures = measures
+                        ctrl.tableView.reloadData()
+                    } else {
+                        self.measures = measures
+                    }
+                }
+            }
+        }
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -38,7 +55,7 @@ class BallotInfoController: UIViewController {
         // Don't try this unless it's loaded, otherwise it will attempt to unwrap optional UI Elements
         if self.isViewLoaded() {
             if let b = self.ballot {
-                self.ballotInfo.text = b.desc   
+                self.ballotInfo.text = b.desc
                 self.ballotTitle.text = b.title
                 self.closingDate.text = b.closingDate.description
             }
@@ -55,5 +72,10 @@ class BallotInfoController: UIViewController {
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        
+        let dest = segue.destinationViewController as! BallotTableController
+        if let measures = self.measures {
+            dest.measures = measures
+        }
     }
 }
