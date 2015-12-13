a4cced32643188764cbda51f8407ea1880147b5f
Caleb Thorsteinson <caleb@thorsteinson.me>
2015-12-12 17:12:36 -0800

    Added note about measure relations

--- VotingApp/MenuController.swift
+++ VotingApp/MenuController.swift
@@ -39,6 +39,7 @@ class MenuController: UIViewController {
             parseQuery.getFirstObjectInBackgroundWithBlock {
                 (object: PFObject?, error: NSError?) -> Void in
                 if let parseBallot = object {
+                    // Note that for the measure relation query, it may make sense to additionally add a filter for measures that the user has already responded to. This could be paired with another query that get's all of the relations they have responded to, in order to create a better UX, and indicate at the ballot table what they have and haven't voted for. Or we could just show the vote status when they load it up and desced deep enough into the view.
                     let measureRelation: PFRelation = parseBallot["measures"] as! PFRelation
                     let closingDate: NSDate = parseBallot["closingDate"] as! NSDate
                     let title: String = parseBallot["title"] as! String
